8db10c7bb33d54551084e908e7c745cbd95028a1
Bruno Paiva <brunovinicius.paiva@gmail.com>
2020-05-10 23:01:43 -0300

    Gui lindo

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Teste
+          Edit <code>src/App.js</code> and save to reload. Gui lindo
         </p>
         <a
           className="App-link"
